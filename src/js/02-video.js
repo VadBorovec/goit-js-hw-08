f5523735a67a82ad37719f1fd2ce84dbaae807a0
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-29 01:08:53 +0300

    add npm vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const TIME_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+//* Save playback time to local storage on timeupdate event
+function getTime({ seconds }) {
+  localStorage.setItem(TIME_KEY, seconds);
+}
+
+player.on('timeupdate', getTime);
+
+//* Resume playback from saved position on page reload
+player.setCurrentTime(parseFloat(localStorage.getItem(TIME_KEY)) || 0);

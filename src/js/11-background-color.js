e91f7fce5327505a474930321891ed6d33978450
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-29 17:57:30 +0300

    delete some colors from random bcg-color

--- src/js/11-background-color.js
+++ src/js/11-background-color.js
@@ -33,15 +33,13 @@ const colors = [
   'linear-gradient(to right, #3d7eaa, #ffe47a)',
   'linear-gradient(to right, #70e1f5, #ffd194)',
   'linear-gradient(to right, #cc95c0, #dbd4b4, #7aa1d2)',
+  'linear-gradient(to right, #cc95c0, #dbd4b4, #7aa1d2)',
   'linear-gradient(to right, #1d976c, #93f9b9)',
-  'linear-gradient(to right, #fc354c, #0abfbc)',
-  'linear-gradient(to right, #fc354c, #0abfbc)',
   'linear-gradient(to right, #fc354c, #0abfbc)',
   'linear-gradient(to right, #fc354c, #0abfbc)',
   'linear-gradient(to right, #fc354c, #0abfbc)',
   'linear-gradient(to right, #e9d362, #333333)',
   'linear-gradient(to right, #f1f2b5, #135058)',
-  'linear-gradient(to right, #237a57, #093028)',
   'linear-gradient(to right, #4ca1af, #2c3e50)',
   'linear-gradient(to right, #be93c5, #7bc6cc)',
   ' linear-gradient(to right, #decba4, #3e5151)',
@@ -53,7 +51,7 @@ const randomColor = colors[randomIndex];
 document.body.style.background = randomColor;
 document.body.style.backgroundSize = 'cover';
 
-console.log(randomColor);
+// console.log(randomColor);
 
 // const colors = [
 //   'linear-gradient(to right, #f4a69a, #f4b0ab, #f4baba, #f4c2ca, #f4cae0)',

8e35ab3da8efe3a2c8bc145d0284a452b6ac3a30
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-03-29 14:53:46 +0000

    Deploying to gh-pages from @ VadBorovec/goit-js-hw-08@ea41252d68072351eb6ebc03384b8fe8275caa05 🚀

---
+++ index.318f11b6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".page-title"),e=document.querySelector(".page-header");document.title=t.textContent,e.textContent=t.textContent;const n=document.querySelector(".back-btn");!function(){"Homework 8"===t.textContent&&n.classList.add("hidden")}();
+//# sourceMappingURL=index.318f11b6.js.map
---
+++ index.81bb9bc4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".page-title"),e=document.querySelector(".page-header");document.title=t.textContent,e.textContent=t.textContent;var n=document.querySelector(".back-btn");!function(){"Homework 8"===t.textContent&&n.classList.add("hidden")}()}();
+//# sourceMappingURL=index.81bb9bc4.js.map
